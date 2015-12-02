853d9c38958eeb5d2b2504e78704995ac31f21d4
Chathuranga Sandun <chathurangasandunkumara@gmail.com>
2015-12-02 23:27:21 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pamba
+ * Date: 10/17/2015
+ * Time: 8:51 PM
+ */
+
+
+$server     = 'localhost';
+$username   = 'root';
+$password   = '';
+$database   = 'map';;
+
+
+$dsn        = "mysql:host=$server;dbname=$database";
